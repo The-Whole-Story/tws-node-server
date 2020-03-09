bea5f4d8877e99465c8b3808cd0bef9acb8d23aa
anacanm <anacan@mangelsdorf.io>
2020-03-09 14:10:57 -0700

    Add initial GET endpoint for trending news
    adds a GET endpoint that will return trending articles
    endpoint might be tailored to specific language or country,
    to be decided after further discussing with group

--- src/services/newsApi.js
+++ src/services/newsApi.js
@@ -8,6 +8,13 @@ const getArticlesByQuery = async (query) => {
     return (await axios.get(url, { params: {}, headers: { Authorization: newsKey } })).data.articles;
 };
 
+const getTrendingArticles = async (query) => {
+    const url = `http://newsapi.org/v2/top-headlines`;
+
+    return (await axios.get(url, { params: {}, headers: { Authorization: newsKey } })).data.articles;
+};
+
 module.exports = {
-    getArticlesByQuery: getArticlesByQuery
+    getArticlesByQuery: getArticlesByQuery,
+    getTrendingArticles: getTrendingArticles
 };
