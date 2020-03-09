834023827850f33d1d85c944574419e6507286f7
anacanm <anacan@mangelsdorf.io>
2020-03-08 21:38:52 -0700

    Change tabs to spaces for readability

--- src/services/newsApi.js
+++ src/services/newsApi.js
@@ -3,11 +3,11 @@ const axios = require('axios');
 const { newsKey } = require('../../config');
 
 const getArticlesByQuery = async (query) => {
-	const url = `http://newsapi.org/v2/everything?q=${query}`;
+    const url = `http://newsapi.org/v2/everything?q=${query}`;
 
-	return (await axios.get(url, { params: {}, headers: { Authorization: newsKey } })).data.articles;
+    return (await axios.get(url, { params: {}, headers: { Authorization: newsKey } })).data.articles;
 };
 
 module.exports = {
-	getArticlesByQuery: getArticlesByQuery
+    getArticlesByQuery: getArticlesByQuery
 };
