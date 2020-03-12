e8bfb356cba2ec2794d23fa251444ca9d08eea09
anacanm <anacan@mangelsdorf.io>
2020-03-11 17:49:42 -0700

    Add GET/politics endpoint, returns political articles
    Adds a GET/politics/:nArticles endpoint that returns the specified
    number of articles, all with a political theme

--- src/routers/aylienRouter.js
+++ src/routers/aylienRouter.js
@@ -3,6 +3,7 @@ const express = require('express');
 const { getBareArticlesByQuery } = require('../services/aylienApi/getBareArticles');
 const { getEntitiesByQuery } = require('../services/aylienApi/getEntities');
 const { getPositiveNews } = require('../services/aylienApi/getPositve');
+const { getPoliticalArticles } = require('../services/aylienApi/getPoliticalArticles');
 
 const router = new express.Router();
 
@@ -50,7 +51,20 @@ router.get('/positive/:query/:nArticles', async (req, res) => {
             res.status(200).send(positiveArticles);
         }
     } catch (err) {
-        console.log(err);
+        res.status(400).send(err);
+    }
+});
+
+router.get('/politics/:nArticles', async (req, res) => {
+    try {
+        const politicalArticles = await getPoliticalArticles(req.params.nArticles);
+
+        if (politicalArticles.length === 0) {
+            res.status(404).send({ error: 'search brought no results' });
+        } else {
+            res.status(200).send(politicalArticles);
+        }
+    } catch (err) {
         res.status(400).send(err);
     }
 });
---
+++ src/services/aylienApi/getPoliticalArticles.js
@@ -0,0 +1,52 @@
+const AylienNewsApi = require('aylien-news-api');
+const { aylienKey, aylienAppId } = require('../../../config');
+
+const defaultClient = AylienNewsApi.ApiClient.instance;
+
+const app_id = defaultClient.authentications['app_id'];
+app_id.apiKey = aylienAppId;
+
+const app_key = defaultClient.authentications['app_key'];
+app_key.apiKey = aylienKey;
+
+const apiInstance = new AylienNewsApi.DefaultApi();
+
+const getPoliticalArticles = async (nArticles) => {
+    const opts = {
+        language: ['en'],
+        sort_by: 'hotness',
+        categoriesTaxonomy: 'iptc-subjectcode',
+        categoriesId: ['06004000', '11000000', '11024000'],
+        perPage: nArticles
+    };
+
+    return await new Promise((resolve, reject) => {
+        apiInstance.listStories(opts, (error, data, response) => {
+            try {
+                let politicalArticles = data.stories.map((story) => {
+                    let obj = {
+                        storyId: story.id,
+                        title: story.title,
+                        author: story.author.name,
+                        body: story.body,
+                        source: {
+                            name: story.source.name,
+                            domain: story.source.domain
+                        },
+                        url: story.links.permalink,
+                        keywords: story.keywords
+                    };
+                    return obj;
+                });
+
+                resolve(politicalArticles);
+            } catch (err) {
+                reject(err);
+            }
+        });
+    });
+};
+
+module.exports = {
+    getPoliticalArticles: getPoliticalArticles
+};
