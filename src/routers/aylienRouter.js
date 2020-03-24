a1fe869293ef0bf4b95569595d4561d1da45843a
anacanm <anacan@mangelsdorf.io>
2020-03-24 12:41:18 -0700

    Add GET/summary endpoint

--- src/routers/aylienRouter.js
+++ src/routers/aylienRouter.js
@@ -3,6 +3,7 @@ const express = require('express');
 const { getArticleIds } = require('../services/aylienApi/getArticleIds');
 const { getArticlesById } = require('../services/aylienApi/getArticlesById');
 const { getSubtopics } = require('../services/aylienApi/getSubtopics');
+const { getSummary } = require('../services/aylienApi/getSummary')
 
 const { auth } = require('../middleware/auth');
 
@@ -24,7 +25,7 @@ router.get('/articleIds', auth, async (req, res) => {
 
 router.get('/articlesById', auth, async (req, res) => {
     try {
-        const ids = Object.values(req.query).map(id => parseInt(id))
+        const ids = Object.values(req.query).map((id) => parseInt(id));
         const articles = await getArticlesById(ids);
         if (articles.length === 0) {
             res.status(404).send({ error: 'search brought no results' });
@@ -46,6 +47,20 @@ router.get('/subtopics', auth, async (req, res) => {
             res.status(200).send(subtopics);
         }
     } catch (err) {
+        res.status(400).send(err);
+    }
+});
+
+router.get('/summary', auth, async (req, res) => {
+    try {
+        const summary = await getSummary(req.query.topic);
+        if (summary === '') {
+            res.status(404).send({ error: 'search brought no results' });
+        } else {
+            res.status(200).send(summary);
+        }
+    } catch (err) {
+        console.log(err)
         res.status(400).send(err);
     }
 });
