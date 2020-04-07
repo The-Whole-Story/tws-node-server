cc731f1cd4496e194da1586a5dec3312e147608a
anacanm <anacan@mangelsdorf.io>
2020-04-07 09:31:55 -0700

    Remove image captions from articles

--- src/services/aylienApi/getArticlesById.js
+++ src/services/aylienApi/getArticlesById.js
@@ -12,34 +12,40 @@ app_key.apiKey = process.env.AYLIEN_KEY;
 
 const apiInstance = new AylienNewsApi.DefaultApi();
 
-const getArticlesById = async (ids) => {
-    if (ids.length < 1 || ids.length > 100) {
-        throw new Error('nArticles must be within 1 and 100, both inclusive');
-    }
-    let opts = {
-        language: ['en'],
-        sort_by: 'recency',
-        id: ids,
-        perPage: ids.length,
-        _return: ["id", "title", "author", "body", "source", "links", "published_at", "keywords"]
-    };
+const isEmpty = (str) => {
+    return str.length === 0 || !str.trim();
+};
 
+const removeImageCaption = (body) => {
+    for (let i = 0; i < body.length; i++) {
+        if (body[i] == 'Image copyright') {
+            body.splice(i, 4);
+        }
+    }
+};
+
+const getArticles = async (opts) => {
     return await new Promise((resolve, reject) => {
         apiInstance.listStories(opts, (error, data, response) => {
             try {
                 const articles = data.stories.map((story) => {
+                    let body = story.body.split('\n').filter((s) => (!isEmpty(s) ? true : false));
+                    //body has split upon new lines and removed paragraphs that have no text
+                    removeImageCaption(body);
+                    //removes image caption
+
                     let obj = {
                         articleId: story.id,
                         title: story.title,
                         author: story.author.name,
-                        body: story.body.split('\n').filter(s => s ? true : false),
+                        body: body,
                         source: {
                             name: story.source.name,
-                            domain: story.source.domain
+                            domain: story.source.domain,
                         },
                         url: story.links.permalink,
                         publishedAt: story.published_at,
-                        keywords: story.keywords
+                        keywords: story.keywords,
                     };
                     return obj;
                 });
@@ -51,6 +57,22 @@ const getArticlesById = async (ids) => {
     });
 };
 
+const getArticlesById = async (ids) => {
+    if (ids.length < 1 || ids.length > 100) {
+        throw new Error('nArticles must be within 1 and 100, both inclusive');
+    }
+    let opts = {
+        language: ['en'],
+        sort_by: 'recency',
+        id: ids,
+        perPage: ids.length,
+        _return: ['id', 'title', 'author', 'body', 'source', 'links', 'published_at', 'keywords'],
+    };
+
+    articles = getArticles(opts);
+    return articles;
+};
+
 module.exports = {
-    getArticlesById: getArticlesById
+    getArticlesById: getArticlesById,
 };
